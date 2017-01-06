9e125e623f392c24fef9ea2fa286ca4a9ec4088a
James Wang <jianwang.academic@gmail.com>
2017-01-06 10:04:16 +0800

    fix company backend declaration

--- packages.el
+++ packages.el
@@ -93,7 +93,7 @@ Each entry is either:
     :defer t
     :init
     (spacemacs|add-company-backends
-      :backends 'company-pfc
+      :backends company-pfc
       :modes pfc-fish-mode)))
 
 (defun pfc-fish/init-enlive ()
